75c8144a340a645eae2156bdd516fa7cc29f7530
stunner15 <nsanjay1505@gmail.com>
2023-10-22 12:21:00 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Form from './components/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Generate Your Resume Here!</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
